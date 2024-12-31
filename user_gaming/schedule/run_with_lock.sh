f5cbdfa445b0d83dfb28e3fcd7f06097d056df5d
apchir-maketech <augustine.chirra@make.tech>
2024-12-30 23:10:31 -0500

    minor fix

--- user_gaming/schedule/run_with_lock.sh
+++ user_gaming/schedule/run_with_lock.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-flock -n /tmp/user_gaming.lock python3 /home/augustine_chirra/pop-marketing-users/user_gaming/main.py 
+flock -n /tmp/user_gaming.lock /home/augustine_chirra/pop-marketing-users/user_gaming/venv/bin/python /home/augustine_chirra/pop-marketing-users/user_gaming/main.py 
